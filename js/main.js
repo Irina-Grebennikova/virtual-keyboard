ef5fca93cc187769e59a637f1f3d1bfcb92dbfe2
Irina <nika_lavrova@internet.ru>
2023-05-04 14:20:17 +0400

    feat: add input in textarea when pressing on keys

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 function createElem(tag, text, parent, ...classes) {
-    let elem = document.createElement(tag);
+    const elem = document.createElement(tag);
     elem.textContent = text;
-    if (classes[0]) classes.forEach(c => elem.classList.add(c));
+    if (classes[0]) classes.forEach((c) => elem.classList.add(c));
     parent.append(elem);
     return elem;
 }
@@ -13,7 +13,7 @@ createElem('main', '', wrapper, '');
 const main = document.querySelector('main');
 
 createElem('h1', 'RSS Виртуальная клавиатура', main, 'title');
-createElem('textarea', '', main, 'input');
+const input = createElem('textarea', '', main, 'input');
 
 createElem('div', '', main, 'keyboard');
 const keyboard = document.querySelector('.keyboard');
@@ -22,59 +22,7 @@ for (let i = 0; i < 5; i++) {
     createElem('div', '', keyboard, 'row');
 }
 
-let rows = document.querySelectorAll('.row');
-
-// function createKeys(indexOfRow, amount) {
-//     for (let j = 0; j < amount; j++) {
-//         let elem = createElem('div', '', rows[indexOfRow], 'key');
-//         if (indexOfRow === 0 && j === 13) {
-//             elem.classList.add('backspace', 'modifier-key');
-//             elem.textContent = 'Backspace';
-//         } else if (indexOfRow === 1 && j === 0) {
-//             elem.classList.add('tab', 'modifier-key');
-//             elem.textContent = 'Tab';
-//         } else if (indexOfRow === 1 && j === 14) {
-//             elem.classList.add('modifier-key');
-//             elem.textContent = 'Del';
-//         } else if (indexOfRow === 2 && j === 0) {
-//             elem.classList.add('caps-lock', 'modifier-key');
-//             elem.textContent = 'Caps-lock';
-//         } else if (indexOfRow === 2 && j === 12) {
-//             elem.classList.add('enter', 'modifier-key');
-//             elem.textContent = 'Enter';
-//         } else if (indexOfRow === 3 && j === 0) {
-//             elem.classList.add('shift', 'modifier-key');
-//             elem.textContent = 'Shift';
-//         } else if (indexOfRow === 3 && j === 11) {
-//             createElem('span', '', elem, 'arrow');
-//         } else if (indexOfRow === 3 && j === 12) {
-//             elem.classList.add('shift', 'modifier-key');
-//             elem.textContent = 'Shift';
-//         } else if (indexOfRow === 4 && j === 0) {
-//             elem.classList.add('ctrl', 'modifier-key');
-//             elem.textContent = 'Ctrl';
-//         } else if (indexOfRow === 4 && j === 1) {
-//             elem.innerHTML = '<svg width="25px" height="25px" viewBox="0 0 32 32" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" fill="#000000"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path fill="#fff" d="M13.821 15.761h-9.038v-7.747l9.038-1.291zM27.217 15.761h-12.185v-9.198l12.185-1.777zM13.821 16.81h-9.038v7.747l9.038 1.291zM27.217 16.81h-12.185v9.198l12.185 1.777z"></path> </g></svg>';
-//         } else if (indexOfRow === 4 && j === 2) {
-//             elem.classList.add('modifier-key');
-//             elem.textContent = 'Alt';
-//         } else if (indexOfRow === 4 && j === 3) {
-//             elem.classList.add('space');
-//         } else if (indexOfRow === 4 && j === 4) {
-//             elem.classList.add('modifier-key');
-//             elem.textContent = 'Alt';
-//         } else if (indexOfRow === 4 && j === 5) {
-//             createElem('span', '', elem, 'arrow', 'arrow-left');
-//         } else if (indexOfRow === 4 && j === 6) {
-//             createElem('span', '', elem, 'arrow', 'arrow-bottom');
-//         } else if (indexOfRow === 4 && j === 7) {
-//             createElem('span', '', elem, 'arrow', 'arrow-right');
-//         } else if (indexOfRow === 4 && j === 8) {
-//             elem.classList.add('ctrl', 'modifier-key');
-//             elem.textContent = 'Ctrl';
-//         }
-//     }
-// }
+const rows = document.querySelectorAll('.row');
 
 const firstRow = rows[0];
 const secondRow = rows[1];
@@ -83,7 +31,7 @@ const fourthRow = rows[3];
 const fifthRow = rows[4];
 
 function createKey(bigEng, lowEng, bigRu, lowRu, parent, ...classes) {
-    let key = createElem('div', '', parent, ...classes);
+    const key = createElem('div', '', parent, ...classes);
     key.dataset.bigEng = bigEng;
     key.dataset.lowEng = lowEng;
     key.dataset.bigRu = bigRu;
@@ -147,9 +95,8 @@ createKey('M', 'm', 'Ь', 'ь', fourthRow, 'key').codeKey = 'KeyM';
 createKey('<', ',', 'Б', 'б', fourthRow, 'key').codeKey = 'Comma';
 createKey('>', '.', 'Ю', 'ю', fourthRow, 'key').codeKey = 'Period';
 createKey('?', '/', ',', '.', fourthRow, 'key').codeKey = 'Slash';
-const arrowTop = createElem('div', '', fourthRow, 'key');
-createElem('span', '', arrowTop, 'arrow');
-arrowTop.codeKey = 'ArrowUp';
+createElem('div', '▲', fourthRow, 'key').codeKey = 'ArrowUp';
+
 createElem('div', 'Shift', fourthRow, 'key', 'shift', 'modifier-key').codeKey = 'ShiftRight';
 
 createElem('div', 'Ctrl', fifthRow, 'key', 'ctrl', 'modifier-key').codeKey = 'ControlLeft';
@@ -159,24 +106,25 @@ win.codeKey = 'MetaLeft';
 createElem('div', 'Alt', fifthRow, 'key', 'modifier-key').codeKey = 'AltLeft';
 createElem('div', '', fifthRow, 'key', 'space').codeKey = 'Space';
 createElem('div', 'Alt', fifthRow, 'key', 'modifier-key').codeKey = 'AltRight';
-const arrowLeft = createElem('div', '', fifthRow, 'key');
-createElem('span', '', arrowLeft, 'arrow', 'arrow-left');
-arrowLeft.codeKey = 'ArrowLeft';
-const arrowBottom = createElem('div', '', fifthRow, 'key');
-createElem('span', '', arrowBottom, 'arrow', 'arrow-bottom');
-arrowBottom.codeKey = 'ArrowDown';
-const arrowRight = createElem('div', '', fifthRow, 'key');
-createElem('span', '', arrowRight, 'arrow', 'arrow-right');
-arrowRight.codeKey = 'ArrowRight';
+createElem('div', '◄', fifthRow, 'key').codeKey = 'ArrowLeft';
+createElem('div', '▼', fifthRow, 'key').codeKey = 'ArrowDown';
+createElem('div', '►', fifthRow, 'key').codeKey = 'ArrowRight';
 createElem('div', 'Ctrl', fifthRow, 'key', 'ctrl', 'modifier-key').codeKey = 'ControlRight';
 
-const charKeys = Array.from(keyboard.querySelectorAll('.key')).filter(key => key.hasAttribute('data-big-eng'));
+const info = createElem('div', '', main, 'info');
+createElem('p', 'Клавиатура создана в операционной системе Windows', info, '');
+
+const keybindingInfo = createElem('p', '', info, '');
+keybindingInfo.innerHTML = 'Для переключения языка комбинация: левыe <b>ctrl</b> + <b>shift</b>';
+
+const arrowKeys = Array.from(keyboard.querySelectorAll('.key')).filter((key) => key.codeKey.includes('Arrow'));
+
+const charKeys = Array.from(keyboard.querySelectorAll('.key')).filter((key) => key.hasAttribute('data-big-eng'));
 
 function generateKeyText(keys, dataAttr) {
-    // console.log('generateKeyText');
-    keys.forEach(key => {
+    keys.forEach((key) => {
         key.textContent = key.dataset[dataAttr];
-    })
+    });
 }
 
 if (sessionStorage.lang === 'ru') {
@@ -185,15 +133,23 @@ if (sessionStorage.lang === 'ru') {
     generateKeyText(charKeys, 'lowEng');
 }
 
-let pressedKey;
+const pressedKeys = [];
 
 document.addEventListener('keydown', (event) => {
-    let allKeys = Array.from(keyboard.querySelectorAll('.key'));
+    if (event.repeat) return;
 
-    for (let key of allKeys) {
+    const allKeys = Array.from(keyboard.querySelectorAll('.key'));
+
+    for (const key of allKeys) {
         if (key.codeKey === event.code) {
             key.classList.add('active');
-            pressedKey = key; 
+            pressedKeys.push(key);
+        }
+    }
+
+    for (const key of charKeys) {
+        if (key.codeKey === event.code) {
+            input.value += key.textContent;
         }
     }
 
@@ -203,12 +159,37 @@ document.addEventListener('keydown', (event) => {
         changeRegister(event);
     } else if (event.key === 'CapsLock') {
         changeLetterCase(event);
+    } else if (event.code === 'Space') {
+        input.value += ' ';
+    } else if (event.key === 'Tab') {
+        input.value += '    ';
+    } else if (event.key === 'Enter') {
+        input.value += '\n';
+    } else if (event.key.includes('Arrow')) {
+        let key;
+        arrowKeys.forEach((el) => {
+            if (el.codeKey === event.code) {
+                key = el;
+            }
+        });
+        input.value += key.textContent;
+    } else if (event.key === 'Delete') {
+        const tmp = input.selectionStart;
+        input.value = input.value.slice(0, tmp) + input.value.slice(tmp + 1);
+        input.selectionEnd = tmp;
+    } else if (event.key === 'Backspace') {
+        input.value = input.value.slice(0, -1);
     }
     event.preventDefault();
 });
 
 document.addEventListener('keyup', (event) => {
-    pressedKey.classList.remove('active');
+    pressedKeys.forEach((key, i) => {
+        if (key.codeKey === event.code) {
+            key.classList.remove('active');
+            pressedKeys.splice(i, 1);
+        }
+    });
 
     if (event.key === 'Shift' && sessionStorage.lang === 'ru') {
         generateKeyText(charKeys, 'lowRu');
@@ -218,8 +199,45 @@ document.addEventListener('keyup', (event) => {
     event.preventDefault();
 });
 
+keyboard.addEventListener('mousedown', (event) => {
+    const key = event.target.closest('.key');
+    if (!key) return;
+
+    if (charKeys.includes(key)) {
+        input.value += key.textContent;
+    } else if (key.codeKey === 'CapsLock') {
+        changeLetterCase(event);
+    } else if (key.codeKey.includes('Shift')) {
+        changeRegister(event);
+    } else if (key.codeKey.includes('Arrow')) {
+        input.value += key.textContent;
+    } else if (event.target.closest('.key') && event.target.closest('.key').textContent === 'Del') {
+        const tmp = input.selectionStart;
+        input.value = input.value.slice(0, tmp) + input.value.slice(tmp + 1);
+        input.selectionEnd = tmp;
+    } else if (key.codeKey === 'Backspace') {
+        input.value = input.value.slice(0, -1);
+    }
+});
+
+keyboard.addEventListener('mouseup', (event) => {
+    const key = event.target.closest('.key');
+    if (!key) return;
+
+    if (key.codeKey.includes('Shift') && sessionStorage.lang === 'ru') {
+        generateKeyText(charKeys, 'lowRu');
+    } else if (key.codeKey.includes('Shift')) {
+        generateKeyText(charKeys, 'lowEng');
+    } else if (key.codeKey === 'Space') {
+        input.value += ' ';
+    } else if (key.codeKey === 'Tab') {
+        input.value += '    ';
+    } else if (key.codeKey === 'Enter') {
+        input.value += '\n';
+    }
+});
+
 function changeLang() {
-    // console.log('changeLang');
     if (charKeys[0].textContent === charKeys[0].dataset.lowEng) {
         generateKeyText(charKeys, 'lowRu');
         sessionStorage.setItem('lang', 'ru');
@@ -230,7 +248,6 @@ function changeLang() {
 }
 
 function changeRegister(event) {
-    // console.log('changeRegister');
     if (event.repeat) return;
 
     if (charKeys[0].textContent === 'ё') {
@@ -241,10 +258,9 @@ function changeRegister(event) {
 }
 
 function changeLetterCase(event) {
-    // console.log('changeLetterCase');
     if (event.repeat) return;
 
-    let letterKeys = charKeys.filter(key => key.textContent.match(/[a-zа-яё]/i));
+    const letterKeys = charKeys.filter((key) => key.textContent.match(/[a-zа-яё]/i));
 
     if (letterKeys[0].textContent === 'ё') {
         generateKeyText(letterKeys, 'bigRu');
@@ -256,26 +272,3 @@ function changeLetterCase(event) {
         generateKeyText(letterKeys, 'lowEng');
     }
 }
-
-
-
-
-
-
-
-
-
-
-createElem('div', '', main, 'info');
-const info = document.querySelector('.info');
-
-createElem('p', 'Клавиатура создана в операционной системе Windows', info, '');
-
-const keybindingInfo = createElem('p', '', info, '');
-keybindingInfo.innerHTML = 'Для переключения языка комбинация: левыe <b>ctrl</b> + <b>shift</b>';
-
-
-
-
-
-
